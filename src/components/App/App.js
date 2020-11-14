c4e04114026e4264b7e4b048a1fcb7dd67e5a807
sergey <dimovitch.sergey@gmail.com>
2020-11-14 13:59:09 +0300

    get products from data to redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,9 +4,20 @@ import ProductForm from "../ProductForm/ProductForm";
 import ProductList from "../ProductList/ProductList";
 import styles from "./App.module.css";
 import axios from "../../utils/axios.utils.js";
+import { getProducts } from '../../actions/products.actions';
+import { useDispatch } from 'react-redux';
 
 function App() {
+const dispatch = useDispatch();
 
+	useEffect(() => {
+		axios.GET("/products").then(response => {	
+			dispatch(getProducts(response.data.products));				
+		}).catch(error =>  {
+			console.log(error);
+		});
+	},[])
+	
  
   return (
     <div className={styles.App}>
