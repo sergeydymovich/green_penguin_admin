201bd48a0d03c26603aa533ba370e8908b77f87b
sergey <dimovitch.sergey@gmail.com>
2020-11-14 17:15:17 +0300

    add delete request to ProductItem

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,11 +1,22 @@
 import React  from 'react';
 import styles from "./ProductItem.module.css";
-
+import axios from "../../utils/axios.utils.js";
 
 function ProductItem({ product }) {
+
+	const deleteProduct = () => {
+
+		axios.DELETE("/products", { id: product._id }).then(() => {
+			
+		}).catch(error =>  {
+			console.log(error);
+		});
+	}
+
   return (
     <li className={styles.item}>
 			<h4>{product.name}</h4>
+			<button className={styles.deleteBtn} onClick={deleteProduct}>УДАЛИТЬ</button>
 		</li>
   );
 }
